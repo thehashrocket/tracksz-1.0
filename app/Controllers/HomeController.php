af82ef54fa5305c1772286768dc9d91a26f97add
Jaymes Sorbel <jaymes@ordello.com>
2020-02-09 18:47:14 -0600

    Add Database

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Library\Config;
 use App\Library\Email;
+use App\Library\ValidateSanitize;
 use App\Library\Views;
 use App\Models\Interested;
 use Laminas\Diactoros\ServerRequest;
@@ -27,8 +28,37 @@ class HomeController
     
     public function interested(ServerRequest $request)
     {
+    
         $form = $request->getParsedBody();
         unset($form['__token']); // remove CSRF token or PDO bind fails, too many arguments, Need to do everytime.
+        
+        // Sanitize and Validate
+        // - Sanitize First to Remove "bad" input
+        // - Validate Second, if Sanitize empties a field due to
+        //   "bad" data that is required then Validate will catch it.
+        $validate = new ValidateSanitize();
+        $form = $validate->sanitize($form); // only sanitizes strings (other filters available)
+    
+        $validate->validation_rules(array(
+            'FullName'    => 'required|alpha_space|max_len,100|min_len,6',
+            'Email'       => 'required|valid_email',
+            'Title'       => 'alpha_space|max_len,25',
+            'Message'     => 'required'
+        ));
+        // Add filters for non-strings (integers, float, emails, etc) ALWAYS Trim
+        $validate->filter_rules(array(
+            'Email'    => 'trim|sanitize_email',
+        ));
+        $validated = $validate->run($form);
+    
+        if ($validated === false) {
+            $validated['alert'] = 'Sorry, we could not add your contact message.  Please try again.';
+            $validated['alert_type'] = 'danger';
+            $this->view->flash($validated);
+            return $this->view->redirect('/#contact');
+        }
+        // if validated !== false then validated has the filtered data.
+        // use validated instead of form now as you see below.
         
         $interest = new Interested($this->db);
         $found = $interest->findEMail($form['Email']);
